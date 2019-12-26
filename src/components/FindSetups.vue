97a140a21bc0ad9327036ca95ccef15d5747108b
Cherezov <victor@mgc.mentorg.com>
2019-12-26 16:54:14 +0300

    find setups v1

--- src/components/FindSetups.vue
+++ src/components/FindSetups.vue
@@ -1,16 +1,60 @@
 <template>
   <div>
     <h3 class="about">This is a Find Setups page</h3>
-    <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
-    <div class="mt-2">Value: {{ text }}</div>
+    <!-- <b-form-input v-model="text" placeholder="Enter your name"></b-form-input> -->
+    <p>Enter Build number and Build tag:</p>
+    <input v-model="build" placeholder="Build" maxlength="4" size="4">
+    <br>
+    <br>
+    <input v-model="tag" placeholder="Tag" maxlength="35" size="35">
+    <hr>
+    <!-- <button v-on:click="clean">Clean</button> -->
+    <b-button variant="outline-primary" v-on:click="clean">Clean</b-button>
+    <hr>
+    <div class="mt-2">Build: {{ build }}</div>
+    <div class="mt-2">Tag: {{ tag }}</div>
+    <hr>
+    <b-button variant="outline-primary" v-on:click="findSetups">Find Setups</b-button>
+    <br>
+    <br>
+    <p>bt: {{ bt }}</p>
+    <p>cfg: {{ cfg }}</p>
+
   </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      text: '',
+      build: '',
+      tag: '',
+      cfg: {},
+      bt: {},
     };
+  },
+  methods: {
+    clean() {
+      this.build = '';
+      this.tag = '';
+      this.bt = {};
+      this.cfg = {};
+    },
+    findSetups() {
+      const path = 'http://127.0.0.1:5000/api/findsetups';
+      this.bt.build = this.build;
+      this.bt.tag = this.tag;
+      axios.post(path, this.bt)
+        .then((res) => {
+          this.cfg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+
   },
 };
 </script>
